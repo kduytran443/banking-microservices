a279de334509db7d71021a0214b18339228a741e
kduytran443 <trankhanhduy18@gmail.com>
2023-11-15 03:01:54 +0700

    Init repositories

---
+++ accounts/src/main/java/com/kduytran/accounts/repository/AccountsRepository.java
@@ -0,0 +1,9 @@
+package com.kduytran.accounts.repository;
+
+import com.kduytran.accounts.entity.Accounts;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountsRepository extends JpaRepository<Accounts, Long> {
+}
