a7af4492f5f300b24e955145b321172d91804c4a
kduytran443 <trankhanhduy18@gmail.com>
2023-11-21 01:20:19 +0700

    AccountsSerivce: init mapper classes

---
+++ accounts/src/main/java/com/kduytran/accounts/mapper/CustomerMapper.java
@@ -0,0 +1,22 @@
+package com.kduytran.accounts.mapper;
+
+import com.kduytran.accounts.dto.CustomerDto;
+import com.kduytran.accounts.entity.Customer;
+
+public class CustomerMapper {
+
+    public static CustomerDto mapToCustomerDto(Customer customer, CustomerDto customerDto) {
+        customerDto.setName(customer.getName());
+        customerDto.setEmail(customer.getEmail());
+        customerDto.setMobileNumber(customer.getMobileNumber());
+        return customerDto;
+    }
+
+    public static Customer mapToCustomer(CustomerDto customerDto, Customer customer) {
+        customer.setName(customerDto.getName());
+        customer.setEmail(customerDto.getEmail());
+        customer.setMobileNumber(customerDto.getMobileNumber());
+        return customer;
+    }
+
+}
