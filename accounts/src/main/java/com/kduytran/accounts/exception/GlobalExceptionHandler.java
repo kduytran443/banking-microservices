ffb69baf77e42d9b773bf9fbecd1df312815c2d3
kduytran443 <trankhanhduy18@gmail.com>
2023-11-26 01:32:07 +0700

    #account-service: Resource not found exception

--- accounts/src/main/java/com/kduytran/accounts/exception/GlobalExceptionHandler.java
+++ accounts/src/main/java/com/kduytran/accounts/exception/GlobalExceptionHandler.java
@@ -23,4 +23,15 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(errorResponseDto, HttpStatus.BAD_REQUEST);
     }
 
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponseDto> handleResourceNotFoundException(ResourceNotFoundException exception, WebRequest webRequest) {
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(
+                webRequest.getDescription(false),
+                HttpStatus.NOT_FOUND,
+                exception.getMessage(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(errorResponseDto, HttpStatus.NOT_FOUND);
+    }
+
 }
