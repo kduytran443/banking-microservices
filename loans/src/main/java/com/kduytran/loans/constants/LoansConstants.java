c151016981f950c73743f4060866977bf7cac355
kduytran443 <trankhanhduy18@gmail.com>
2023-11-26 22:51:55 +0700

    Create constants

---
+++ loans/src/main/java/com/kduytran/loans/constants/LoansConstants.java
@@ -0,0 +1,17 @@
+package com.kduytran.loans.constants;
+
+public class LoansConstants {
+    public LoansConstants() {
+        //Cannot create instance of this class
+    }
+    public static final String LOAN_TYPE = "";
+    public static final String STATUS_201 = "201";
+    public static final String MESSAGE_201 = "Loan created successfully";
+    public static final String STATUS_200 = "200";
+    public static final String MESSAGE_200 = "Request processed successfully";
+    public static final String STATUS_417 = "417";
+    public static final String MESSAGE_417_UPDATE = "Update operation failed. Please try again or contact Dev team";
+    public static final String MESSAGE_417_DELETE = "Delete operation failed. Please try again or contact Dev team";
+    public static final String STATUS_500 = "500";
+    public static final String MESSAGE_500 = "An error occurred. Please try again or contact Dev team";
+}
