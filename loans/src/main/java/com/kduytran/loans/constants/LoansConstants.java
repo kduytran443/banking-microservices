6e6fd154fec5186b922132f3ce0606ca399769c9
kduytran443 <trankhanhduy18@gmail.com>
2023-11-27 00:22:37 +0700

    Update constant

--- loans/src/main/java/com/kduytran/loans/constants/LoansConstants.java
+++ loans/src/main/java/com/kduytran/loans/constants/LoansConstants.java
@@ -4,7 +4,8 @@ public class LoansConstants {
     public LoansConstants() {
         //Cannot create instance of this class
     }
-    public static final String LOAN_TYPE = "";
+    public static final String  HOME_LOAN = "Home Loan";
+    public static final int  NEW_LOAN_LIMIT = 1_00_000;
     public static final String STATUS_201 = "201";
     public static final String MESSAGE_201 = "Loan created successfully";
     public static final String STATUS_200 = "200";
