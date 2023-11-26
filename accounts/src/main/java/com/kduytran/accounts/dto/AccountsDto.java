259aa05b6762a9c0c86839265983d1b13804a8a9
kduytran443 <trankhanhduy18@gmail.com>
2023-11-26 14:30:33 +0700

    Spring DOC

--- accounts/src/main/java/com/kduytran/accounts/dto/AccountsDto.java
+++ accounts/src/main/java/com/kduytran/accounts/dto/AccountsDto.java
@@ -1,5 +1,6 @@
 package com.kduytran.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
@@ -7,13 +8,22 @@ import lombok.Data;
 @Data
 public class AccountsDto {
 
+    @Schema(
+            description = "Account number of the Account", example = "1011011023"
+    )
     @NotEmpty(message = "Account number cannot be null or empty")
     @Pattern(regexp = "(^\\w{10}$)", message = "Account number must be 10 digits")
     private Long accountNumber;
 
+    @Schema(
+            description = "Account Type of the Account", example = "saving"
+    )
     @NotEmpty(message = "Account Type cannot be null or empty")
     private String accountType;
 
+    @Schema(
+            description = "Branch address of the Account", example = "123 Main Street, New York"
+    )
     @NotEmpty(message = "Branch address cannot be null or empty")
     private String branchAddress;
 }
--- accounts/src/main/java/com/kduytran/accounts/dto/ResponseDto.java
+++ accounts/src/main/java/com/kduytran/accounts/dto/ResponseDto.java
@@ -1,10 +1,24 @@
 package com.kduytran.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+@Schema(
+        name = "Response",
+        description = "Schema to hold successful response information"
+)
 @Data @AllArgsConstructor
 public class ResponseDto {
-    private String status;
-    private String message;
+
+    @Schema(
+            description = "Status code in the response", example = "200"
+    )
+    private String statusCode;
+
+    @Schema(
+            description = "Status message in the response", example = "Request processed successfully"
+    )
+    private String statusMsg;
+
 }
