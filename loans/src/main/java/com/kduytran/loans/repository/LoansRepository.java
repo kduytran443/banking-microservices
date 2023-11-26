29d5ab3674b1551ee7cbf811a2e57cd9e461c88f
kduytran443 <trankhanhduy18@gmail.com>
2023-11-27 00:23:09 +0700

    Update repository

--- loans/src/main/java/com/kduytran/loans/repository/LoansRepository.java
+++ loans/src/main/java/com/kduytran/loans/repository/LoansRepository.java
@@ -4,7 +4,10 @@ import com.kduytran.loans.entity.Loans;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface LoansRepository extends JpaRepository<Loans, Long> {
-
+    Optional<Loans> findByMobileNumber(String MobileNumber);
+    void deleteByMobileNumber(String MobileNumber);
 }
