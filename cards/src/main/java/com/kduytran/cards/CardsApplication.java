ea7a4184b2d39bce035e257549a1637bbd5370e0
kduytran443 <trankhanhduy18@gmail.com>
2023-12-17 23:52:24 +0700

    Using @Value, Environment interface and @ConfigurationProperties to read configurations

--- cards/src/main/java/com/kduytran/cards/CardsApplication.java
+++ cards/src/main/java/com/kduytran/cards/CardsApplication.java
@@ -1,14 +1,17 @@
 package com.kduytran.cards;
 
+import com.kduytran.cards.dto.CardsContactInfoDto;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties({CardsContactInfoDto.class})
 @OpenAPIDefinition(
 		info = @Info(
 				title = "Cards microservice REST API Documentation",
