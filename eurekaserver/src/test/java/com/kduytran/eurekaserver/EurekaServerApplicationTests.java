96947f2b7880bdf7fcc83ed1180b5461ae77ab84
kduytran443 <trankhanhduy18@gmail.com>
2024-01-22 01:25:23 +0700

    Init Eureka Server

---
+++ eurekaserver/src/test/java/com/kduytran/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kduytran.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
