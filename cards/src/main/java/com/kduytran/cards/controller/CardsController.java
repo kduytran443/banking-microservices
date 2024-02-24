d409c3f51190fcd9b7f41450ef5ef872fe05f25e
kduytran443 <trankhanhduy18@gmail.com>
2024-02-24 22:48:36 +0700

    Update docker compose

--- cards/src/main/java/com/kduytran/cards/controller/CardsController.java
+++ cards/src/main/java/com/kduytran/cards/controller/CardsController.java
@@ -26,8 +26,8 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(
-        name = "CRUD REST APIs for Cards in EazyBank",
-        description = "CRUD REST APIs in EazyBank to CREATE, UPDATE, FETCH AND DELETE card details"
+        name = "CRUD REST APIs for Cards in Bank",
+        description = "CRUD REST APIs in Bank to CREATE, UPDATE, FETCH AND DELETE card details"
 )
 @RestController
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
@@ -54,7 +54,7 @@ public class CardsController {
 
     @Operation(
             summary = "Create Card REST API",
-            description = "REST API to create new Card inside EazyBank"
+            description = "REST API to create new Card inside Bank"
     )
     @ApiResponses({
             @ApiResponse(
