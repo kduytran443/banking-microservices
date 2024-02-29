75cbf4353a7b88792eb71624c7b492fb5d4a380e
kduytran443 <trankhanhduy18@gmail.com>
2024-03-01 00:00:20 +0700

    Custom configuration for TimeLimiter

--- gatewayserver/src/main/java/com/kduytran/gatewayserver/config/RouteConfig.java
+++ gatewayserver/src/main/java/com/kduytran/gatewayserver/config/RouteConfig.java
@@ -1,6 +1,11 @@
 package com.kduytran.gatewayserver.config;
 
 import com.kduytran.gatewayserver.utils.PathUtils;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -46,5 +51,13 @@ public class RouteConfig {
                         .uri("lb://LOANS"))
                 .build();
     }
+
+    @Bean
+    public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
+        return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+                .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+                .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(4)).build()).build());
+    }
+
 
 }
