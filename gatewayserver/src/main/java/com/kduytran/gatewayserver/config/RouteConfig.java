213add4327478e4db5bae8c87e1c9054afb78c5e
kduytran443 <trankhanhduy18@gmail.com>
2024-02-29 22:11:31 +0700

    Implementing Retry pattern in Gateway for Accounts Service

--- gatewayserver/src/main/java/com/kduytran/gatewayserver/config/RouteConfig.java
+++ gatewayserver/src/main/java/com/kduytran/gatewayserver/config/RouteConfig.java
@@ -5,7 +5,9 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 @Configuration
@@ -22,6 +24,10 @@ public class RouteConfig {
                                 .circuitBreaker(config -> config.setName("accountsCircuitBreaker")
                                         .setFallbackUri("forward:/contactSupport")
                                 )
+                                .retry(retryConfig -> retryConfig
+                                        .setRetries(3)
+                                        .setMethods(HttpMethod.GET)
+                                        .setBackoff(Duration.ofMillis(100), Duration.ofMillis(1000), 2, true))
                         )
                         .uri("lb://ACCOUNTS"))
                 .route(p -> p
