107af74fb30f439d75deb171fe3ba10829df8e43
kduytran443 <trankhanhduy18@gmail.com>
2024-03-04 00:45:48 +0700

    Implementing RateLimiter in Accounts Server

--- accounts/src/main/java/com/kduytran/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/kduytran/accounts/controller/AccountsController.java
@@ -6,6 +6,7 @@ import com.kduytran.accounts.dto.CustomerDto;
 import com.kduytran.accounts.dto.ErrorResponseDto;
 import com.kduytran.accounts.dto.ResponseDto;
 import com.kduytran.accounts.service.IAccountsService;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -205,9 +206,15 @@ public class AccountsController {
                     )
             )
     })
+    @RateLimiter(name = "getJavaVersion", fallbackMethod = "getJavaVersionFallback")
     @GetMapping("/java-version")
     public ResponseEntity<String> getJavaVersion() {
         return ResponseEntity.status(HttpStatus.OK).body(environment.getProperty("JAVA_HOME"));
+    }
+
+
+    public ResponseEntity<String> getJavaVersionFallback(Throwable throwable) {
+        return ResponseEntity.status(HttpStatus.OK).body("Java 17");
     }
 
     @Operation(
