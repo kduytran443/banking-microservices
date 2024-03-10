c3e43b4553321ebc8396c3d087b1bf0130f26c79
kduytran443 <trankhanhduy18@gmail.com>
2024-03-10 23:50:51 +0700

    remove redis rate limiter

--- gatewayserver/src/main/java/com/kduytran/gatewayserver/config/RouteConfig.java
+++ gatewayserver/src/main/java/com/kduytran/gatewayserver/config/RouteConfig.java
@@ -3,12 +3,9 @@ package com.kduytran.gatewayserver.config;
 import com.kduytran.gatewayserver.utils.PathUtils;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 import io.github.resilience4j.timelimiter.TimeLimiterConfig;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
 import org.springframework.cloud.client.circuitbreaker.Customizer;
-import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
-import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -20,15 +17,6 @@ import java.time.LocalDateTime;
 
 @Configuration
 public class RouteConfig {
-
-    private final RedisRateLimiter redisRateLimiter;
-    private final KeyResolver userKeyResolver;
-
-    @Autowired
-    public RouteConfig(RedisRateLimiter redisRateLimiter, KeyResolver userKeyResolver) {
-        this.redisRateLimiter = redisRateLimiter;
-        this.userKeyResolver = userKeyResolver;
-    }
 
     @Bean
     public RouteLocator routeLocator(RouteLocatorBuilder routeLocatorBuilder) {
@@ -52,9 +40,6 @@ public class RouteConfig {
                         .filters(f -> f
                                 .rewritePath(PathUtils.getPathWithContextPath("cards/(?<segment>.*)"), "/${segment}")
                                 .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
-                                .requestRateLimiter(config -> config
-                                        .setRateLimiter(redisRateLimiter)
-                                        .setKeyResolver(userKeyResolver))
                         )
                         .uri("lb://CARDS"))
                 .route(p -> p
